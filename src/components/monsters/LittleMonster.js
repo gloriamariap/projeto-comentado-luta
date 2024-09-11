9d689b616b844fbe20f27213936b38e9150281c2
Glória Maria <160123526+gloriamariap@users.noreply.github.com>
2024-09-11 08:41:01 -0300

    Add files via upload

---
+++ src/components/monsters/LittleMonster.js
@@ -0,0 +1,9 @@
+class LittleMonster extends Character {
+    constructor(){
+        super('Little Monster')
+        this.life = 80
+        this.attack = 10
+        this.defense = 8
+        this.maxLife = this.life
+    }
+}
